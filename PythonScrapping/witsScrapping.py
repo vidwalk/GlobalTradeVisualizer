c6dc3c71076adfeda0d44d51cfd944d4a75c59e7
vidwalk <claudiurediu@gmail.com>
2020-03-19 22:13:13 +0100

    WITS API get on specific hs code

--- PythonScrapping/witsScrapping.py
+++ PythonScrapping/witsScrapping.py
@@ -5,19 +5,18 @@ from bs4 import BeautifulSoup as Soup
 from requests_html import HTMLSession
 import ssl
 import certifi
+import pandas as pd
 # declare a session object
 session = HTMLSession()
 user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Brave Chrome/78.0.3904.108 Safari/537.36'
 headers = {'User-Agent': user_agent}
 
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-        getattr(ssl, '_create_unverified_context', None)):
-    ssl._create_default_https_context = ssl._create_unverified_context
-
+# The verify is a huge problem
 responseHTML = session.get(
-    'https://wits.worldbank.org/trade/comtrade/en/country/ALL/year/2018/tradeflow/Exports/partner/WLD/product/030270#', headers=headers, cert=certifi.where())
+    'https://wits.worldbank.org/trade/comtrade/en/country/ALL/year/2018/tradeflow/Exports/partner/WLD/product/030270#', headers=headers, verify=False)
 soup = Soup(responseHTML.content, 'html5lib')
-f = open("result.txt", "a")
-f.write(str(soup))
+#f = open("result.txt", "a")
+# f.write()
 # f.write(str(responseHTML.html.xpath('//*[@id="productTitle"]//text()')))
-f.close()
+# f.close()
+print(pd.read_html(responseHTML.content, header=0))
